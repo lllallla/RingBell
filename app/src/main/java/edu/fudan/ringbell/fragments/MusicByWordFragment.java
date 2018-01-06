ccf9b20fbb3a5a8ea5a8e7e3a3a04c07428d9c44
Niu1234 <635449644@qq.com>
2018-01-07 04:38:21 +0800

    页面 音乐列表实现

--- app/src/main/java/edu/fudan/ringbell/fragments/MusicByWordFragment.java
+++ app/src/main/java/edu/fudan/ringbell/fragments/MusicByWordFragment.java
@@ -2,7 +2,6 @@ package edu.fudan.ringbell.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,24 +21,29 @@ import edu.fudan.ringbell.media.MediaUtil;
  */
 
 public class MusicByWordFragment extends Fragment {
-    private String tag = "musicByword";
     private ListView mMusiclist;                   //音乐列表
-    private MusicAdapter mAdapter;
+    private SimpleAdapter mAdapter;
     List<MusicInfo> musicInfos = null;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // inflater.inflate(resource, null);
+
         View view = inflater.inflate(R.layout.fragment_mr_word, container, false);
         mMusiclist = (ListView) view.findViewById(R.id.musiclistbyword);
         //为ListView添加数据源
         musicInfos = MediaUtil.getMusicInfos(getActivity().getApplicationContext());  //获取歌曲对象集合
-        mAdapter = new MusicAdapter(getActivity(),musicInfos);
-        Log.d(tag,"--------------------");
-        Log.d(tag,String.valueOf(musicInfos.size()));
-        mMusiclist.setAdapter(mAdapter);
+        setListAdpter(MediaUtil.getMusicMaps(musicInfos));    //显示歌曲列表
         return view;
     }
 
 
+    public void setListAdpter(List<HashMap<String, String>> musiclist) {
+        mAdapter = new SimpleAdapter(getActivity(), musiclist,
+                R.layout.music_item, new String[]{"number","title","check_music",
+                "Artist", "music_menu"}, new int[]{R.id.number,R.id.music_title,R.id.check,
+                R.id.music_Artist, R.id.music_menu});
+        mMusiclist.setAdapter(mAdapter);
+    }
 
 }
