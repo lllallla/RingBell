3045affb92c1423cad6d0d64fb75ef474da7dc08
lllallla <14302010051@fudan.edu.cn>
2018-01-10 18:03:09 +0800

    实现工具类

---
+++ app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/edu_fudan_ringbell_media_MusicConverter.o.d
@@ -0,0 +1,113 @@
+D:/AndroidStudioProjects/RingBell/app/src/main/obj/local/armeabi-v7a/objs/ffmpeginvoke/edu_fudan_ringbell_media_MusicConverter.o: \
+  edu_fudan_ringbell_media_MusicConverter.c \
+  edu_fudan_ringbell_media_MusicConverter.h ffmpeg.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\config.h cmdutils.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavcodec/avcodec.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/samplefmt.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/avutil.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/common.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/attributes.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/macros.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/version.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/avconfig.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/mem.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/error.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/rational.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/mathematics.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/intfloat.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/log.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/pixfmt.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/buffer.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/cpu.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/channel_layout.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/dict.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/frame.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavcodec/version.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavfilter/avfilter.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavfilter/version.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavformat/avformat.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavformat/avio.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavformat/version.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libswscale/swscale.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libswscale/version.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/eval.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/fifo.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/threadmessage.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libswresample/swresample.h \
+  C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libswresample/version.h logjam.h
+
+edu_fudan_ringbell_media_MusicConverter.h:
+
+ffmpeg.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\config.h:
+
+cmdutils.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavcodec/avcodec.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/samplefmt.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/avutil.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/common.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/attributes.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/macros.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/version.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/avconfig.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/mem.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/error.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/rational.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/mathematics.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/intfloat.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/log.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/pixfmt.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/buffer.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/cpu.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/channel_layout.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/dict.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/frame.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavcodec/version.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavfilter/avfilter.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavfilter/version.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavformat/avformat.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavformat/avio.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavformat/version.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libswscale/swscale.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libswscale/version.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/eval.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/fifo.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libavutil/threadmessage.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libswresample/swresample.h:
+
+C:/Users/Lenovo/Desktop/ffmpeg-3.2.4\libswresample/version.h:
+
+logjam.h:
