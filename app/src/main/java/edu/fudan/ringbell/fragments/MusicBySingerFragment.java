3e446a8cf4382f60c0dc8afa662fd912c42d475d
Niu1234 <635449644@qq.com>
2018-01-10 18:29:13 +0800

    页面 修改实现    背景设置在backgroundstyle里

--- app/src/main/java/edu/fudan/ringbell/fragments/MusicBySingerFragment.java
+++ app/src/main/java/edu/fudan/ringbell/fragments/MusicBySingerFragment.java
@@ -1,75 +1,40 @@
 package edu.fudan.ringbell.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.util.Log;
+import android.widget.ListView;
 
 import edu.fudan.ringbell.R;
+import edu.fudan.ringbell.adapter.MusicAdapter;
+import edu.fudan.ringbell.entity.MusicInfo;
+import edu.fudan.ringbell.media.MediaUtil;
 
-import java.util.ArrayList;
+
+import java.util.List;
 
 /**
  * Created by niuzhenghao on 2017/11/27.
  */
 
 public class MusicBySingerFragment extends Fragment {
-//
-//    private static final String TAG = "MusicBySingerFragment";
-//    private RecyclerView recyclerView;
-//    private SingerAdapter adapter;
-//    private List<SingerInfo> singerInfoList = new ArrayList<>();
-//    private DBManager dbManager;
-//    private Context mContext;
-//
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        this.mContext = context;
-//    }
-//
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        singerInfoList.clear();
-//        singerInfoList.addAll(MyMusicUtil.groupBySinger((ArrayList) dbManager.getAllMusicFromMusicTable()));
-//        Log.d(TAG, "onResume: singerInfoList.size() = "+singerInfoList.size());
-//        adapter.notifyDataSetChanged();
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        super.onCreateView(inflater, container, savedInstanceState);
-//        Log.d("aaaa", "onCreateView: ");
-//        View view = inflater.inflate(R.layout.fragment_singer,container,false);
-//        recyclerView = (RecyclerView)view.findViewById(R.id.singer_recycler_view);
-//        dbManager = DBManager.getInstance(getContext());
-//        Log.e(TAG, "SingerFragment: singerInfoList.size() ="+ singerInfoList.size());
-//        adapter = new SingerAdapter(getContext(),singerInfoList);
-//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-//        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-//        recyclerView.setLayoutManager(linearLayoutManager);
-//        recyclerView.setAdapter(adapter);
-//        adapter.setOnItemClickListener(new SingerAdapter.OnItemClickListener() {
-//        @Override
-//        public void onDeleteMenuClick(View content, int position) {
-//            Log.d(TAG, "onDeleteMenuClick: ");
-//        }
-//
-//        @Override
-//        public void onContentClick(View content, int position) {
-//            Log.d(TAG, "onContentClick: ");
-//            Intent intent = new Intent(mContext,ModelActivity.class);
-//            intent.putExtra(ModelActivity.KEY_TITLE,singerInfoList.get(position).getName());
-//            intent.putExtra(ModelActivity.KEY_TYPE, ModelActivity.SINGER_TYPE);
-//            mContext.startActivity(intent);
-//        }
-//    });
-//        return view;
-//}
+    private ListView mMusiclist;                   //音乐列表
+    private MusicAdapter mAdapter;
+    List<MusicInfo> musicInfos = null;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // inflater.inflate(resource, null);
+
+        View view = inflater.inflate(R.layout.fragment_mr_singer, container, false);
+        mMusiclist = (ListView) view.findViewById(R.id.musiclistbysinger);
+        //为ListView添加数据源
+        musicInfos = MediaUtil.getMusicInfos(getActivity().getApplicationContext());  //获取歌曲对象集合
+        mAdapter =  new MusicAdapter(this.getActivity(),musicInfos);
+        mMusiclist.setAdapter(mAdapter);
+        return view;
+    }
+
 }
