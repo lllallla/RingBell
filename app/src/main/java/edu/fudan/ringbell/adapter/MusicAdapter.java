48dca53f022e74cd8755794664ee7ab8989b08ee
febend <tqyao14@fudan.edu.cn>
2018-01-11 22:05:41 +0800

    complete  request

--- app/src/main/java/edu/fudan/ringbell/adapter/MusicAdapter.java
+++ app/src/main/java/edu/fudan/ringbell/adapter/MusicAdapter.java
@@ -3,6 +3,7 @@ package edu.fudan.ringbell.adapter;
 import android.app.AlertDialog;
 import android.content.Context;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,12 +14,15 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.File;
 import java.util.List;
 
+import edu.fudan.ringbell.MakeRIngActivity;
 import edu.fudan.ringbell.PlayMusicActivity;
 import edu.fudan.ringbell.R;
 import edu.fudan.ringbell.entity.MusicInfo;
 import edu.fudan.ringbell.fragments.BottomSheetFragment;
+import edu.fudan.ringbell.http.FileRequest;
 
 
 /**
@@ -72,10 +76,32 @@ public class MusicAdapter extends BaseAdapter {
             holder.menu.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-//                    BottomSheetFragment fragment = BottomSheetFragment.newInstance(holder.title.getText().toString());
-////                    context.getSupportFragmentManager().
-//                    fragment.show(context.getSupportFragmentManager(),BottomSheetFragment.class.getSimpleName());
-
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context,android.R.style.Theme_Holo_Light_Dialog);
+                    //builder.setIcon(R.drawable.ic_launcher);
+                    builder.setTitle("选择您的操作");
+                    //    指定下拉列表的显示数据
+                    final String[] cities = {"收藏铃声", "编辑铃声", "铃声设置", "闹钟设置", "铃声分享","铃声删除"};
+                    //    设置一个下拉的列表选择项
+                    builder.setItems(cities, new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            switch (which) {
+                                case 1:
+                                    Intent intent = new Intent(context, MakeRIngActivity.class);
+                                    intent.putExtra("path", musciList.get(Integer.parseInt((String)holder.number.getText()) - 1).getUrl());
+                                    context.startActivity(intent);
+                                case 4 :
+                                    int i = FileRequest.upload(new File(musciList.get(Integer.valueOf((String)holder.number.getText()) - 1).getUrl()),holder.title.getText().toString());
+                                    break;
+                                case 5:
+                                    int j = FileRequest.delete(holder.title.getText().toString());
+                                    break;
+                            }
+                        }
+                    });
+                    builder.show();
                 }
             });
             MusicListener=new MusicListener(position);
